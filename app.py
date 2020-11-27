74bf93722179e30a6c5927f82a32971719f648d5
Abdulkadir Ozpolat <abdulkadirozpolatt@gmail.com>
2020-11-27 23:03:54 +0300

    configurated sqlalchemy and postgresql

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+import psycopg2
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:test@localhost/HW2'
+db=SQLAlchemy(app)
+
+      
 @app.route('/')
 def login():
    return render_template('login.html')
