f87f6b141eff99e6ba2bcb5a0d86ea25ee0ddbb9
ibahadiraltun <ibahadiraltun@gmail.com>
2020-11-29 21:12:00 +0300

    set up frontend for user-conference requests

--- app.py
+++ app.py
@@ -3,17 +3,35 @@ from flask_sqlalchemy import SQLAlchemy
 import psycopg2
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:test@localhost/HW2'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ibahadiraltun:@localhost/bil372-hw2'
 db=SQLAlchemy(app)
 
-      
+user_requests = []
+for i in range(0, 20):
+    user_requests.append({
+        'name': 'user' + str(i),
+        'id': i
+    })
+
+conf_requests = []
+for i in range(0, 20):
+    conf_requests.append({
+        'name': 'conf' + str(i),
+        'id': i
+    })
+
 @app.route('/')
 def login():
-   return render_template('login.html')
+    return render_template('login.html')
 
 @app.route('/signup')
 def signup():
-   return render_template('signup.html')   
+    return render_template('signup.html')   
+
+@app.route('/main')
+def main():
+    return render_template('main.html', users = user_requests, confs = conf_requests)
+
 
 if __name__ == '__main__':
    app.run(debug = True)
